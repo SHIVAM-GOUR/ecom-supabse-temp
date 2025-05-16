80ab21fbce98ce4089e811c7c11f2c4ef3176a5e
Riya <riya.garg@data-route.com>
2025-05-16 14:30:53 +0530

    input added

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -14,6 +14,7 @@ import { useEffect, useState } from 'react'
 
 export default function Page() {
   const [notes, setNotes] = useState<any[] | null>(null)
+  const [carName, setCarName] = useState('')
   const supabase = createClient()
 
   useEffect(() => {
@@ -25,12 +26,21 @@ export default function Page() {
   }, [])
 
   const updateCar = async () => {
-    await supabase.from("car").update({ name: "Riya's defender ❤️" }).eq('id', 1).select();
+    await supabase.from("car").update({ name: carName}).eq('id', 1).select();
+    const { data } = await supabase.from('car').select('id, name').order('id', { ascending: true })
+    setNotes(data)
   }
 
-  return <div>
+  return <div className='flex flex-col'>
     <h1>Update</h1>
-    <button className='bg-purple-400 p-2 rounded-sm shadow-sm' onClick={() => updateCar()}>Update Car name</button>
+ <input
+        type="text"
+        placeholder="Enter new car name"
+        value={carName}
+        onChange={(e) => setCarName(e.target.value)}
+        className="p-2"
+      />
+    <button className='bg-purple-400 p-2 rounded-sm shadow-sm my-4' onClick={() => updateCar()}>Update Car name</button>
     <pre>{JSON.stringify(notes, null, 2)}</pre>
   </div>
 }
